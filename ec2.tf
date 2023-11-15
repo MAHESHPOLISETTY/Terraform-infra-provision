34cd69f2b28dfcf0a3fea921554b5ad4e15b716f
EC2 Default User <ec2-user@ip-172-31-27-180.eu-north-1.compute.internal>
2023-11-15 04:54:54 +0000

    task3

--- ec2.tf
+++ ec2.tf
@@ -13,14 +13,14 @@ resource "aws_instance" "prj2_mahi_web_instance" {
 
 ###RDS_SERVER_INSTANCE######
 
-resource "aws_instance" "prj2_mahi_rds_instance" {
-  ami                    = "ami-0e8a34246278c21e4"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.prj2_mahi_pvt_sub.id
-  key_name               = "prj2"
-  count                  = 1
-  vpc_security_group_ids = [aws_security_group.prj2_mahi_rds.id]
+#########resource "aws_instance" "prj2_mahi_rds_instance" {##
+ ##ami                    = "ami-0e8a34246278c21e4"
+##  instance_type          = "t2.micro"
+  ##subnet_id              = aws_subnet.prj2_mahi_pvt_sub.id
+  ##key_name               = "prj2"
+  ##count                  = 1
+  ##vpc_security_group_ids = [aws_security_group.prj2_mahi_rds.id]
 
-}
+#}##
 
 
