ed90fbf8eff612cc264f9a3b0942073a66716edd
MAHESHPOLISETTY <138252706+MAHESHPOLISETTY@users.noreply.github.com>
2023-11-21 08:59:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
 
   region = "us-east-1"
 
-  access_key = "AKIA4Y4E4VJE2FEOLG5Z"
+  access_key = "accesskey"
 
-  secret_key = "DOEM4cTumMf5c+jgwuHiW9BnalCEkZnVJ4kH9GlL"
+  secret_key = "<secretkey>"
 
 }
 
